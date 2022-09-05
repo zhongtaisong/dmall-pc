6cf5d0350638c750ab389b8bd44b5ab736414986
zhongtaisong <zhongtaisong@sina.com>
2022-09-05 23:21:26 +0800

    【update - 商品详情】取消赞、踩

--- src/pages/goods-detail/components/commodity-evaluation/index.tsx
+++ src/pages/goods-detail/components/commodity-evaluation/index.tsx
@@ -1,8 +1,6 @@
-import React, { createElement } from 'react';
-import { Comment, Avatar, Empty, Tooltip, } from 'antd';
+import React from 'react';
+import { Comment, Avatar, Empty, } from 'antd';
 import { observer } from 'mobx-react';
-import { DislikeFilled, DislikeOutlined, LikeFilled, LikeOutlined } from '@ant-design/icons';
-import lodash from 'lodash';
 // 设置
 import { PUBLIC_URL } from '@config';
 // mobx数据
@@ -10,31 +8,13 @@ import store from '@store';
 // less样式
 import './index.less';
 
-interface IComponentState {
-    /**
-     * 当前操作，赞、踩
-     */
-    actionKeyMap: {
-        [key: string]: "liked" | "disliked";
-    };
-}
-
 /**
  * 商品评价
  */
 @observer
-class CommodityEvaluation extends React.PureComponent<any, IComponentState> {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            actionKeyMap: {},
-        };
-    }
-
+class CommodityEvaluation extends React.PureComponent<any, any> {
     render() {
         const { evaluationList } = store?.goodsDetailStore || {};
-        const { actionKeyMap } = this.state;
         if(!Array.isArray(evaluationList) || !evaluationList.length) {
             return (
                 <Empty image={ Empty.PRESENTED_IMAGE_SIMPLE } description='暂无评价' />
@@ -49,28 +29,6 @@ class CommodityEvaluation extends React.PureComponent<any, IComponentState> {
                             <Comment
                                 key={ item?.id }
                                 className="commodity_evaluation__comment"
-                                actions={[
-                                    <Tooltip 
-                                        key="agree"
-                                        className="commodity_evaluation__comment--action"
-                                        title="赞"
-                                    >
-                                        <span onClick={() => this.onActionClick(item?.id, 'liked')}>
-                                            { createElement(actionKeyMap[item?.id] === 'liked' ? LikeFilled : LikeOutlined) }
-                                            <span className="commodity_evaluation__comment--action__number">{ item?.agree || 0 }</span>
-                                        </span>
-                                    </Tooltip>,
-                                    <div 
-                                        key="disagree"
-                                        className="commodity_evaluation__comment--action"
-                                        title="踩"
-                                    >
-                                        <span onClick={() => this.onActionClick(item?.id, 'disliked')}>
-                                            { createElement(actionKeyMap[item?.id] === 'disliked' ? DislikeFilled : DislikeOutlined) }
-                                            <span className="commodity_evaluation__comment--action__number">{ item?.disagree || 0 }</span>
-                                        </span>
-                                    </div>
-                                ]}
                                 author={ item?.uname }
                                 avatar={ <Avatar src={ `${ PUBLIC_URL }${ item?.avatar }` } alt="用户头像" /> }
                                 content={ <div className='commodity_evaluation__comment--content'>{ item.content }</div> }
@@ -81,24 +39,6 @@ class CommodityEvaluation extends React.PureComponent<any, IComponentState> {
                 }
             </div>
         );
-    }
-
-    /**
-     * 点赞、踩 - 操作
-     * @param id 
-     * @param key 
-     */
-    onActionClick = (id, key) => {
-        let { actionKeyMap } = this.state;
-        actionKeyMap = lodash.cloneDeep(actionKeyMap);
-        const actionKeyMapItem = actionKeyMap[id];
-        if(key === actionKeyMapItem) {
-            delete actionKeyMap[id];
-        }else {
-            actionKeyMap[id] = key;
-        }
-
-        this.setState({ actionKeyMap, });
     }
 }
 
